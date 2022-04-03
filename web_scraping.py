f9e28d6d1731d8e022b4602cc9f65e80d816db5d
OrlandoG88 <98724886+OrlandoG88@users.noreply.github.com>
2022-04-03 14:01:41 -0400

    first commit

---
+++ web_scraping.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+page = requests.get('https://en.wikipedia.org/wiki/List_of_World_Series_of_Poker_Main_Event_champions')
+
+soup = BeautifulSoup(page.text, 'html.parser')
+results_table = soup.find_all('table', class_= 'wikitable sortable')
+rows = results_table[0].find_all('tr')
+headers = rows[0].find_all('th')
+
+print(f'{headers[1].text.strip():<8} - {headers[2].text.strip():<8} - {headers[3].text.strip():<8}')
+
+
+for row in rows:
+    cells = row.find_all('td')
+    #if not cells:
+        #continue
+
+print(f'{cells[1].text.strip():<8} - {cells[2].text.strip():<8} - {cells[3].text.strip():<8}')
+
+
+
+
